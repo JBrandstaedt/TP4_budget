9ff15c65b0d0872852bcb6221facf110f60bb2a5
jbrandstaedt <jules.brandstaedt@etu.unistra.fr>
2023-11-15 16:12:01 -0500

    solved models.py error

--- myscript.py
+++ myscript.py
@@ -1,3 +1,4 @@
 import os
-os.system('git bisect start 9c611f83bf13ece1ce57195d0e8f63f70fef46af e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c')
+os.system('git bisect start 9c611f83bf13ece1ce57195d0e8f63f70fef46af \
+          e4cfc6f77ebbe2e23550ddab682316ab4ce1c03c')
 os.system('git bisect run python manage.py test')
